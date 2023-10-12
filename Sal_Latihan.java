42f5dc0d8b8868d791829de7743d07a0850052ab
CeryOstrich <140327345+CeryOstrich@users.noreply.github.com>
2023-10-11 19:26:59 -0700

    Add files via upload
    tugas java melakukan penjumlahan tanggal 12 oktober 2023

---
+++ Sal_Latihan.java
@@ -0,0 +1,13 @@
+public class Sal_Latihan {
+    public static void main(String[]args) {
+        //mendefinisikan dua angka yang dijumlahkan
+        int angka1 = 5;
+        int angka2 = 7;
+
+        //melakukan penjumlahan
+        int hasil = angka1 + angka2;
+
+        //menampilkan hasil penjumlahan ke layar
+        System.out.println("Hasil penjumlahan " + angka1 + " dan " + angka2 + " adalah " +hasil);
+    }
+}
