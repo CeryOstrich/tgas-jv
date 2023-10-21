7e74ae5990cc340235adb48c379e7454c00e3e14
CeryOstrich <140327345+CeryOstrich@users.noreply.github.com>
2023-10-21 14:26:20 +0800

    Add files via upload
    contoh percabangan pemrograman java sabtu 21 oktober 2023

---
+++ ContohPercabangan.java
@@ -0,0 +1,19 @@
+public class ContohPercabangan {
+    public static void main(String[] args) {
+        int pilihan = 2;
+
+        switch (pilihan) {
+            case 1:
+                System.out.println("Anda memilih opsi 1.");
+                break;
+            case 2:
+                System.out.println("Anda memilih opsi 2.");
+                break;
+            case 3:
+                System.out.println("Anda memilih opsi 3.");
+                break;
+            default:
+                System.out.println("Pilihan tidak valid.");
+        }
+    }
+}
