1a0ca1feaaf08219ccdd739b83f9db660846b3e5
CeryOstrich <140327345+CeryOstrich@users.noreply.github.com>
2023-10-21 14:08:34 +0800

    Add files via upload
    Contoh kasus java sabtu 21 oktober 2023 tentang percabangan

---
+++ percabangan.java
@@ -0,0 +1,13 @@
+public class percabangan {
+public static void main(String[] args) {
+    int angka = 10;
+
+    if (angka > 0) {
+        System.out.println("Angka adalah positif.");
+    } else if (angka < 0) {
+        System.out.println("Angka adalah negatif.");
+    } else {
+        System.out.println("Angka adalah nol.");
+    }
+}
+}
